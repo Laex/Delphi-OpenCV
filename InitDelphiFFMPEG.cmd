620d6011540de37331f83dfe603e056e3de02ac7
Laentir Valetov <laex@bk.ru>
2020-02-21 22:07:22 +0400

    Added *.res files
    Signed-off-by: Laentir Valetov <laex@bk.ru>

--- InitDelphiFFMPEG.cmd
+++ InitDelphiFFMPEG.cmd
@@ -1,2 +1,19 @@
+@echo off
+rem Get the submodule initially
 @git submodule update --init
 @git submodule update
+
+rem Time passes, submodule upstream is updated
+rem and you now want to update
+
+rem Change to the submodule directory
+cd Delphi-FFMPEG
+
+rem Checkout desired branch
+git checkout master
+
+rem Update
+git pull
+
+rem Get back to your project root
+cd ..
